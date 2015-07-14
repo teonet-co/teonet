b7d5492150c7c9f912c7d904abf07d5e9f87fc4f
Kirill Scherba <kirill@scherba.ru>
2015-07-14 10:03:06 +0300

    Empty teovpn application created

---
+++ app/teovpn.c
@@ -0,0 +1,21 @@
+/** 
+ * File:   teovpn.c
+ * Author: Kirill Scherba
+ *
+ * Created on July 14, 2015, 9:52 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TVPN_VERSION "0.0.2"
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    printf("Teovpn ver " TVPN_VERSION "\n");
+    
+    return (EXIT_SUCCESS);
+}
