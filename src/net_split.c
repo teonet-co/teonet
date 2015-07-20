4ed564ee78802181906c22cd753b43a6d7b992af
kirill <kirill@scherba.ru>
2015-07-20 14:25:00 +0300

    The split / combine packets was done and debugged Time and colors was added to log output

--- src/net_split.c
+++ src/net_split.c
@@ -11,6 +11,7 @@
 #include <string.h>
 
 #include "net_split.h"
+#include "utils/rlutil.h"
 
 #define kev ((ksnetEvMgrClass*)(((ksnCoreClass*)(ks->kco->kc))->ke))
 
@@ -85,8 +86,8 @@ void **ksnSplitPacket(ksnSplitClass *ks, uint8_t cmd, void *packet, size_t packe
 
         #ifdef DEBUG_KSNET
         ksnet_printf(&kev->ksn_cfg, DEBUG_VV,
-            "ksnSplitPacket: %d bytes packet was splitted to %d subpackets\n",
-            (int)packet_len, *num_subpackets);
+            "%sksnSplitPacket:%s %d bytes packet was splitted to %d subpackets\n",
+            ANSI_BLUE, ANSI_NONE, (int)packet_len, *num_subpackets);
         #endif
     }
 
@@ -184,8 +185,8 @@ ksnCorePacketData *ksnSplitCombine(ksnSplitClass *ks, ksnCorePacketData *rd) {
 
         #ifdef DEBUG_KSNET
         ksnet_printf(&kev->ksn_cfg, DEBUG_VV,
-            "ksnSplitCombine: combine %d subpackets to large %d bytes packet\n",
-            subpacket_num+1, (int)data_len);
+            "%sksnSplitCombine:%s combine %d subpackets to large %d bytes packet\n",
+            ANSI_BLUE, ANSI_NONE, subpacket_num+1, (int)data_len);
         #endif
 
         // Create new ksnCorePacketData for combined block
