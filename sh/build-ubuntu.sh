d69e5d18087e4018f19e24e1f323bd09e657434d
Kirill Scherba <kirill@scherba.ru>
2015-07-30 02:05:00 +0300

    The libtuntab was added to project distributive

--- sh/build-ubuntu.sh
+++ sh/build-ubuntu.sh
@@ -10,8 +10,8 @@ sudo apt-get install -y libssl-dev libev-dev libconfuse-dev uuid-dev
 sudo apt-get install -y cmake g++ 
 
 # Get libtuntap
-git clone git@gitlab.ksproject.org:ksnet/ksmesh.git #http://gitlab.ksproject.org/ksnet/ksmesh.git
-cd ksmesh/distr
+#git clone git@gitlab.ksproject.org:ksnet/ksmesh.git #http://gitlab.ksproject.org/ksnet/ksmesh.git
+cd distr
 unzip libtuntap.zip
 cd libtuntap-master
 
@@ -21,6 +21,6 @@ make
 sudo make install
 
 # Remove libtuntap source
-cd ../..
-rm -fr ksmesh
+cd ..
+#rm -fr ksmesh
 
