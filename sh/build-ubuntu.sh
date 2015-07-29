8c7f9e977f6d93c058b3c4c1891e84bab1b4f892
Kirill Scherba <kirill@scherba.ru>
2015-07-30 01:43:45 +0300

    The file build-ubuntu.sh was added

---
+++ sh/build-ubuntu.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Autoconf dependence
+sudo apt-get -y install autoconf intltool libtool libglib2.0-dev doxygen make gcc
+
+# Project dependence
+sudo apt-get install -y libssl-dev libev-dev libconfuse-dev uuid-dev
+
+# Libtuntap dependence
+sudo apt-get install -y cmake g++ 
+
+# Get libtuntap
+http://gitlab.ksproject.org/ksnet/ksmesh.git
+cd ksmesh/distr
+unzip libtuntap.zip
+cd libtuntap-master
+
+# Libtuntap build
+cmake ./
+make
+sudo make install
+
+# Remove libtuntap source
+cd ../..
+rm -fr ksmesh
+
