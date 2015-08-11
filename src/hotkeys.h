6e31fc9add212e742fb36b76d3397cab2fa09ea6
Kirill Scherba <kirill@scherba.ru>
2015-08-11 17:35:32 +0300

    Issue #35: The continously show of TR-UDP statistic in hotkey monitor done

--- src/hotkeys.h
+++ src/hotkeys.h
@@ -46,6 +46,21 @@ typedef struct monitor_timer_data {
 } monitor_timer_data;
 
 /**
+ * TR UDP timer data
+ */
+typedef struct tr_udp_timer_data {
+
+    struct ev_loop *loop;
+    ksnCoreClass *kn;
+    int num_lines;
+    ev_timer tw;
+    ev_idle iw;
+    size_t num;
+
+} tr_udp_timer_data;
+
+
+/**
  * Peer timer data
  */
 typedef struct peer_timer_data {
@@ -73,13 +88,15 @@ typedef struct ksnetHotkeysClass  {
     #endif
     int wait_y; ///< Wait y (yes) mode
     int peer_m; ///< Show peer mode: 0 - single; 1 - continously
+    int tr_udp_m; ///< Show tr-udp mode: 0 - single; 1 - continously
     int last_hotkey; ///< Last hotkey
     int str_number; ///< Nuber of current string
     char str[3][KSN_BUFFER_SM_SIZE]; ///< Strings
 
-    ping_timer_data *pt;
-    monitor_timer_data *mt;
-    peer_timer_data *pet;
+    ping_timer_data *pt; ///< Hotkey Pinger timer data
+    monitor_timer_data *mt; ///< Hotkey Monitor timer data
+    peer_timer_data *pet; ///< Hotkey Peer timer data
+    tr_udp_timer_data *put; ///< Hotkey Peer timer data
     
     ev_io stdin_w; ///< STDIN watcher
     ev_idle idle_stdin_w; ///< Idle STDIN watcher
