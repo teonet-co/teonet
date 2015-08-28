29f6c50bb16e8870d221cc40e07dc2c643c3ae75
Kirill Scherba <ka.scherba@gbstudios.ru>
2015-08-28 19:13:41 +0300

    Issue #60: The script to create RPM was updated, it can create REHL RPM and upload it to remote server (7)

--- sh/make_remote_upload.sh
+++ sh/make_remote_upload.sh
@@ -8,6 +8,8 @@
 # Created on Aug 27, 2015, 2:08:45 AM
 #
 
+set -e # exit at error
+
 ANSI_BROWN="\033[22;33m"
 ANSI_NONE="\033[0m"
 
@@ -15,8 +17,8 @@ REPO=../repo
 
 # Parameters:
 #
-# @param $1 RPM_SUBTYPE: deb, rpm, yum, zyp
-# @param $2 Install prefix: sudo apt-get install -y 
+# @param $1 RPM_SUBTYPE: deb, rpm, yum, zyp (default: deb)
+# @param $2 Install prefix: default: sudo apt-get install -y 
 #
 
 # Upload local repository to remote host
@@ -32,20 +34,16 @@ if [ -z "$2" ];  then
 else
     $2" lftp"
 fi
+if [ "$RPM_SUBTYPE" = "deb" ]; then
+  SUBFOLDER=ubuntu
+else
+  SUBFOLDER=rhel
+fi
 lftp -c "
 set ftp:list-options -a;
 open ftp://repo:VV9x5ClC@repo.ksproject.org; 
-if [ "$RPM_SUBTYPE" = "deb" ]
-  then
-
-    lcd $REPO/ubuntu;
-    cd /ubuntu;
-
-  else
-
-    lcd $REPO/rhel;
-    cd /rhel;
-fi
+lcd $REPO/$SUBFOLDER;
+cd /$SUBFOLDER;
 mirror --reverse --delete --use-cache --verbose --allow-chown
 "
 echo ""
