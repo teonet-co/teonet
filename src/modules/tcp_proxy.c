7e19415994da530ba46fc7b63c032f7d49f0d864
Kirill Scherba <ka.scherba@gbstudios.ru>
2015-09-08 14:01:21 +0300

    Issue #26: TCP module code structure defined

--- src/modules/tcp_proxy.c
+++ src/modules/tcp_proxy.c
@@ -2,13 +2,16 @@
  * \file   tcp_proxy.c
  * \author Kirill Scherba <kirill@scherba.ru>
  * 
- * Teonet TCP Proxy module
+ * # Teonet TCP Proxy module
  * 
- * See example: @ref teotproxy.c
+ * See example: 
  * 
  * See test: test_tcp_proxy.c
  *
- * Created on September 8, 2015, 1:59 AM
+ * *Created on September 8, 2015, 1:59 AM*
+ * 
+ * ### Description:
+ * 
  */
 
 #include <stdlib.h>
@@ -46,6 +49,55 @@ void ksnTCPProxyDestroy(ksnTCPProxyClass *tp) {
 
 // Common functions -----------------------------------------------------------
 
-// Client functions -----------------------------------------------------------
+//! \file   tcp_proxy.c
+//! * Client functions: 
 
-// Server functions -----------------------------------------------------------
+//! \file   tcp_proxy.c
+//!     * Connect to TCP Proxy server: ksnTCPProxyConnetc()
+
+/**
+ * Connect to TCP Proxy server
+ * 
+ * @param tp Pointer to ksnTCPProxyClass
+ * @return 0 - Successfully connected
+ */
+int ksnTCPProxyConnetc(ksnTCPProxyClass *tp) {
+   
+    // Get address and port from teonet config
+    
+    // Connect to R-Host TCP Server
+    
+    // Register connection (in ARP ?)
+    
+    // \todo
+    
+    return 0;
+}
+
+//! \file   tcp_proxy.c
+//! * Server functions: 
+
+//! \file   tcp_proxy.c
+//!     * Start TCP Proxy server
+
+/**
+ * Start TCP Proxy server
+ * 
+ * @param tp
+ * @return 
+ */
+int ksnTCPProxyServerStart(ksnTCPProxyClass *tp) {
+    
+    // \todo
+    
+    return 0;
+}
+
+//! \file   tcp_proxy.c
+//!     * When client is connected to TCP Proxy server: 
+//!
+//! \file   tcp_proxy.c
+//!         * Register connection 
+//!         * Start UDP port Proxy
+//!         * Resend datagrams between TCP Server and UDP port Proxy 
+
