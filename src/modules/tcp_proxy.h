5dd89ab1a6bda5355d528741f5db68bd43b33410
Kirill Scherba <kirill@scherba.ru>
2015-09-09 19:20:40 +0300

    Issue #26: TCP Proxy basic server functions created

--- src/modules/tcp_proxy.h
+++ src/modules/tcp_proxy.h
@@ -10,15 +10,31 @@
 #ifndef TCP_PROXY_H
 #define	TCP_PROXY_H
 
+#include <stdio.h>
+#include <pbl.h>
+#include <ev.h>
+
 /**
  * TCP Proxy class data
  */
 typedef struct ksnTCPProxyClass {
     
-    void *ke;   ///< Pointer to ksnetEvMgrClass
-    int fd;     ///< TCP Server fd or 0 if not started
+    void *ke;    ///< Pointer to ksnetEvMgrClass
+    int fd;      ///< TCP Server fd or 0 if not started
+    PblMap* map; ///< Hash Map to store tcp proxy client connections
     
 } ksnTCPProxyClass;
+
+/**
+ * ksnTCPProxyClass map data
+ */
+typedef struct ksnTCPProxyData {
+    
+    int udp_proxy_port; ///< UDP Proxy port number
+    int udp_proxy_fd;   ///< UDP Proxy file descriptor
+    ev_io w;            ///< TCP Client watcher
+    
+} ksnTCPProxyData;
 
 #ifdef	__cplusplus
 extern "C" {
