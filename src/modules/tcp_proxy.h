133f9aa22a2d6146bf6c7e127ab50d64d7c24c10
Kirill Scherba <kirill@scherba.ru>
2015-09-14 18:45:35 +0300

    Issue #26: The Package Create and Package Process functions was created

--- src/modules/tcp_proxy.h
+++ src/modules/tcp_proxy.h
@@ -10,9 +10,36 @@
 #ifndef TCP_PROXY_H
 #define	TCP_PROXY_H
 
+#include <stdint.h>
 #include <stdio.h>
 #include <pbl.h>
 #include <ev.h>
+
+#include "config/conf.h"
+
+/**
+ * TCP Proxy buffer stage
+ */
+typedef enum {
+    
+    WAIT_FOR_BEGAN, ///< Wait for packet beginning
+    WAIT_FOR_END, ///< Wait for end of packet
+    PROCESS_PACKET ///< Process the packet in buffer
+    
+} ksnTCPProxyBufferStage;
+
+/**
+ * TCP Proxy packet(message) header structure
+ */
+typedef struct ksnTCPProxyMessage_header {
+    
+    uint8_t checksum; ///< Checksum
+    unsigned int version : 4; ///< Protocol version number    
+    unsigned int addr_len : 4; ///< UDP peers address string length included trailing zero  
+    uint16_t port; ///< UDP peers port number
+    uint16_t package_len; ///< UDP package length
+    
+} ksnTCPProxyMessage_header;
 
 /**
  * TCP Proxy class data
@@ -22,6 +49,9 @@ typedef struct ksnTCPProxyClass {
     void *ke;    ///< Pointer to ksnetEvMgrClass
     int fd;      ///< TCP Server fd or 0 if not started
     PblMap* map; ///< Hash Map to store tcp proxy client connections
+    char *buffer[KSN_BUFFER_DB_SIZE]; ///< TCP Proxy packet buffer
+    size_t ptr;  ///< TCP Proxy packet buffer
+    int stage;   ///< TCP Proxy packet buffer
     
 } ksnTCPProxyClass;
 
