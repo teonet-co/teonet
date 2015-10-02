e717a77eed92f01ffad55cc6cfa76d7db827b168
Kirill Scherba <kirill@scherba.ru>
2015-10-02 12:55:33 +0300

    Issue #90: Move event_cb to main teoweb module

--- app/teoweb.c
+++ app/teoweb.c
@@ -18,6 +18,56 @@
 #define TWEB_VERSION "0.0.1"
 
 /**
+ * Teonet event handler
+ *
+ * @param ke
+ * @param event
+ * @param data
+ * @param data_len
+ * @param user_data
+ */
+void event_cb(ksnetEvMgrClass *ke, ksnetEvMgrEvents event, void *data,
+              size_t data_len, void *user_data) {
+
+    // Switch Teonet events
+    switch(event) {
+
+        case EV_K_ASYNC:
+            {
+                struct mg_connection *nc = user_data;
+                struct teoweb_data *td = data;
+                
+                if(data != NULL) {
+                    
+                    switch(td->cmd) {
+                        
+                        case WS_CONNECTED:
+                            printf("Async event was received from %p, "
+                                "connected\n", nc);
+                            break;
+                            
+                        case WS_DISCONNECTED:
+                            printf("Async event was received from %p, "
+                                "disconnected\n", nc);
+                            break;
+                            
+                        case WS_MESSAGE: 
+                            printf("Async event was received from %p, "
+                                   "%d bytes: '%.*s'\n", 
+                                   nc, (int)data_len, (int)td->data_len, 
+                                   (char*) td->data);
+                            break;
+                    }
+                }                
+            }
+            break;
+            
+        default:
+            break;
+    }
+}
+
+/**
  * Main application function
  *
  * @param argc
@@ -29,7 +79,7 @@ int main(int argc, char** argv) {
     printf("Teoweb ver " TWEB_VERSION ", based on teonet ver " VERSION "\n");
     
     // Initialize teonet event manager and Read configuration
-    ksnetEvMgrClass *ke = ksnetEvMgrInit(argc, argv, ksnHTTPEventCb /*NULL*/, READ_ALL);
+    ksnetEvMgrClass *ke = ksnetEvMgrInit(argc, argv, event_cb /*NULL*/, READ_ALL);
     
     // Start HTTP server
     ksnHTTPClass *kh = ksnHTTPInit(ke);
