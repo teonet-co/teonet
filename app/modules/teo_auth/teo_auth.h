3077add5001dfdd114be7a49b53db194ae801b7c
Kirill Scherba <kirill@scherba.ru>
2015-12-01 15:25:00 +0300

    Issue #132: The teo_auth module created

---
+++ app/modules/teo_auth/teo_auth.h
@@ -0,0 +1,25 @@
+/** 
+ * File:   teo_auth.h
+ * Author: Kirill Scherba <kirill@scherba.ru>
+ * 
+ * Module to connect to Authentication server AuthPrototype
+ *
+ * Created on December 1, 2015, 1:52 PM
+ */
+
+#ifndef TEO_AUTH_H
+#define	TEO_AUTH_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TEO_AUTH_H */
+
