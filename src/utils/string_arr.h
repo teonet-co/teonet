fbe4c7065a91eee73fc32db34558214137d426a5
Kirill Scherba <kirill@scherba.ru>
2016-02-10 21:09:49 +0300

    The string array module updated

--- src/utils/string_arr.h
+++ src/utils/string_arr.h
@@ -20,11 +20,8 @@ int             ksnet_stringArrLength(ksnet_stringArr arr);
 ksnet_stringArr ksnet_stringArrFree(ksnet_stringArr *arr);
 ksnet_stringArr	ksnet_stringArrSplit(const char* string, const char* separators,
                                 int with_empty, int max_parts);
-char *ksnet_stringArrCombine(ksnet_stringArr arr);
-
-char *remove_extras(char *str, const char *extras);
-char find_replace_to_char(const char *str, const char *replace_to_str);
-int replaceCharBetweenQuotas(char *str, char replace_from, char replace_to);
+char *ksnet_stringArrCombine(ksnet_stringArr arr, const char* separator);
+int ksnet_stringArrMoveTo(ksnet_stringArr arr, unsigned int fromIdx, unsigned int toIdx);
 
 #ifdef	__cplusplus
 }
