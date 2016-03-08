d80839b9b6a773aaa000eabb57d1f74b9aeb739a
Kirill Scherba <kirill@scherba.ru>
2016-03-08 05:10:46 +0300

    allow reset command from l0 clients to peers

--- src/modules/l0-server.c
+++ src/modules/l0-server.c
@@ -604,7 +604,8 @@ int cmd_l0_cb(ksnetEvMgrClass *ke, ksnCorePacketData *rd) {
     ksnLNullSPacket *data = rd->data;
         
     // Process command
-    if(data->cmd == CMD_ECHO || data->cmd == CMD_PEERS || data->cmd == CMD_L0_CLIENTS ||
+    if(data->cmd == CMD_ECHO || data->cmd == CMD_PEERS || 
+       data->cmd == CMD_L0_CLIENTS || data->cmd == CMD_RESET ||
        data->cmd == CMD_SUBSCRIBE || data->cmd == CMD_L0_CLIENTS_N || 
        data->cmd == CMD_L0_STAT || data->cmd == CMD_HOST_INFO ||
        data->cmd == CMD_GET_NUM_PEERS ||
