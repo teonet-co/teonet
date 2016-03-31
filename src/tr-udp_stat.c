8688fe24f49029b1858a37a76021ad2b727726c4
Kirill Scherba <kirill@scherba.ru>
2016-03-31 18:28:52 +0300

    reset TR-UDP if max_count = max_value (2000) and attempt > 5 (4, calculation of triptime_last_max corrected)

--- src/tr-udp_stat.c
+++ src/tr-udp_stat.c
@@ -299,8 +299,8 @@ void ksnTRUDPsetACKtime(ksnTRUDPClass *tu, __CONST_SOCKADDR_ARG addr,
 
         // Add to last 10 array
         ip_map_d->stat.triptime_last_ar[ip_map_d->stat.idx] = ip_map_d->stat.triptime_last;
-        if(ip_map_d->stat.idx >= LAST10_SIZE) ip_map_d->stat.idx = 0;
-        else ip_map_d->stat.idx++;
+        if(ip_map_d->stat.idx < LAST10_SIZE) ip_map_d->stat.idx++;
+        else ip_map_d->stat.idx = 0;
 
         // Calculate max in last 10 packet
         ip_map_d->stat.triptime_last_max = 0;
