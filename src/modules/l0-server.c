1b042da291d33e1a1d3d3c97b62eb39e1a61c633
max <mpano91@gmail.com>
2019-10-15 21:01:59 +0300

    Fix command condition

--- src/modules/l0-server.c
+++ src/modules/l0-server.c
@@ -895,7 +895,7 @@ int cmd_l0_cb(ksnetEvMgrClass *ke, ksnCorePacketData *rd) {
     // Process command
     if(data->cmd == CMD_ECHO || data->cmd == CMD_ECHO_ANSWER ||
        data->cmd == CMD_PEERS || data->cmd == CMD_L0_CLIENTS ||
-       data->cmd == CMD_RESET || data->cmd == CMD_SUBSCRIBE || data->cmd == CMD_SUBSCRIBE_RND || CMD_UNSUBSCRIBE ||
+       data->cmd == CMD_RESET || data->cmd == CMD_SUBSCRIBE || data->cmd == CMD_SUBSCRIBE_RND || data->cmd == CMD_UNSUBSCRIBE ||
        data->cmd == CMD_L0_CLIENTS_N || data->cmd == CMD_L0_STAT ||
        data->cmd == CMD_HOST_INFO || data->cmd == CMD_GET_NUM_PEERS ||
        data->cmd == CMD_TRUDP_INFO ||
