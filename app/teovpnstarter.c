3620af8f400d4f723782ddfa13b4fa87f78d5eae
Kirill Shokhin <k_shokhin@wargaming.net>
2020-10-12 16:13:27 +0300

    curl cb can be called multiple times. Reduce number of allocations a bit.

--- app/teovpnstarter.c
+++ app/teovpnstarter.c
@@ -14,23 +14,90 @@ typedef struct AppSettings
     char *public_ipv6;
 } AppSettings;
 
-size_t save_data_cb(void *ptr, size_t size, size_t nmemb, void *stream){
-    int buffer_size = nmemb + 1;
-    char** buffer_ptr = (char**)stream;
+typedef struct Buffer
+{
+    char *data;
+    size_t size;
+    size_t capacity;
+} Buffer;
 
-    *buffer_ptr = (char*)malloc(buffer_size);
-    memcpy(*buffer_ptr, ptr, nmemb);
-    (*buffer_ptr)[buffer_size - 1] = '\0';
+Buffer create_buffer() {
+    Buffer buf;
+
+    buf.data = NULL;
+    buf.size = 0;
+    buf.capacity = 0;
+
+    return buf;
+}
+
+void destroy_buffer(Buffer *buffer) {
+    free(buffer->data);
+    buffer->data = NULL;
+    buffer->size = 0;
+    buffer->capacity = 0;
+}
+
+void clear_buffer(Buffer *buffer) {
+    buffer->size = 0;
+}
+
+char* stealFromBuffer(Buffer *buffer) {
+    char* data = buffer->data;
+
+    buffer->data = NULL;
+    buffer->capacity = 0;
+    buffer->size = 0;
+
+    return data;
+}
+
+void set_buffer(Buffer *buffer, const char* data, size_t data_size) {
+    if (buffer == NULL || data == NULL || data_size == 0) { return; }
+
+    free(buffer->data);
+
+    buffer->data = (char*)malloc(data_size);
+    memcpy(buffer->data, data, data_size);
+    buffer->size = data_size;
+    buffer->capacity = data_size;
+}
+
+void append_to_buffer(Buffer *buffer, const char* data, size_t data_size) {
+    if (buffer == NULL || data == NULL || data_size == 0) { return; }
+
+    if (buffer->data == NULL) {//NOTE: empty buffer, just copy data
+        set_buffer(buffer, data, data_size);
+    } else {
+        size_t old_capacity = buffer->capacity;
+        while(buffer->capacity - buffer->size < data_size) {
+            buffer->capacity *= 2;
+        }
+
+        if (old_capacity != buffer->capacity) {
+            //TOOD: realloc can fail
+            buffer->data = (char*)realloc(buffer->data, buffer->capacity);
+        }
+
+        memcpy(buffer->data + buffer->size, data, data_size);
+        buffer->size += data_size;
+    }
+}
+
+size_t save_data_cb(void *ptr, size_t size, size_t nmemb, void *stream){
+    Buffer *buffer = (Buffer*)stream;
+
+    append_to_buffer(buffer, ptr, nmemb);
 
     return nmemb;
 }
 
 int request_public_ipv4(CURL *curl, AppSettings *settings) {
-    char *buffer = NULL;
+    Buffer data_buffer = create_buffer();
 
     curl_easy_setopt(curl, CURLOPT_URL, "http://169.254.169.254/latest/meta-data/public-ipv4");
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, save_data_cb);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &data_buffer);
 
     /* Perform the request, res will get the return code */
     CURLcode res = curl_easy_perform(curl);
@@ -44,21 +111,22 @@ int request_public_ipv4(CURL *curl, AppSettings *settings) {
     curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
 
     if (response_code == 200) {
-        settings->public_ipv4 = strdup(buffer);
+        append_to_buffer(&data_buffer, "", 1);//NOTE: add terminating null
+        settings->public_ipv4 = stealFromBuffer(&data_buffer);
     } else {
         settings->public_ipv4 = NULL;
     }
 
-    free(buffer);
+    destroy_buffer(&data_buffer);
 
     return 1;
 }
 
 int request_public_ipv6(CURL *curl, AppSettings *settings) {
-    char *buffer = NULL;
+    Buffer data_buffer = create_buffer();
     curl_easy_setopt(curl, CURLOPT_URL, "http://169.254.169.254/latest/meta-data/network/interfaces/macs/");
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, save_data_cb);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &data_buffer);
 
     /* Perform the request, res will get the return code */
     CURLcode res = curl_easy_perform(curl);
@@ -68,22 +136,23 @@ int request_public_ipv6(CURL *curl, AppSettings *settings) {
         return 0;
     }
 
+    //TODO:check http code
+
     char *ipv6_url_request_buffer = NULL;
     const char* ipv6_url_pattern = "http://169.254.169.254/latest/meta-data/network/interfaces/macs/ipv6s";
     //NOTE: buffer holds string like 02:43:8a:02:50:57/, so it already has '/'
-    size_t ipv6_url_buffer_size = strlen(ipv6_url_pattern) + strlen(buffer) + 1;
+    size_t ipv6_url_buffer_size = strlen(ipv6_url_pattern) + data_buffer.size + 1;
+    append_to_buffer(&data_buffer, "", 1);//NOTE: add terminating null
     ipv6_url_request_buffer = (char*)malloc(ipv6_url_buffer_size);//http://169.254.169.254/latest/meta-data/network/interfaces/macs/mac-address/ipv6s
     snprintf(ipv6_url_request_buffer, ipv6_url_buffer_size,
-        "http://169.254.169.254/latest/meta-data/network/interfaces/macs/%sipv6s", buffer);
-    free(buffer);
-
-    buffer = NULL;
+        "http://169.254.169.254/latest/meta-data/network/interfaces/macs/%sipv6s", data_buffer.data);
+    clear_buffer(&data_buffer);
 
     printf("%s\n", ipv6_url_request_buffer);
 
     curl_easy_setopt(curl, CURLOPT_URL, ipv6_url_request_buffer);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, save_data_cb);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &data_buffer);
 
     res = curl_easy_perform(curl);
     if (res != CURLE_OK) {
@@ -95,16 +164,17 @@ int request_public_ipv6(CURL *curl, AppSettings *settings) {
     long response_code = 0;
     curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
 
-    printf("%s\n%ld\n", buffer, response_code);
+    printf("%ld\n", response_code);
 
     if (response_code == 200) {
-        settings->public_ipv6 = strdup(buffer);
+        append_to_buffer(&data_buffer, "", 1);//NOTE: add terminating null
+        settings->public_ipv6 = stealFromBuffer(&data_buffer);
     } else {
         settings->public_ipv6 = NULL;
     }
 
     free(ipv6_url_request_buffer);
-    free(buffer);
+    destroy_buffer(&data_buffer);
 
     return 1;
 }
@@ -118,7 +188,7 @@ void launch_app(AppSettings* settings, int argc, char** argv) {
 
     const char* home_val = getenv(home_env_var);
     printf("%s\n", home_val);
-    size_t buf_size = strlen(home_env_var) + 1 + strlen(home_val) + 1;//HOME=/bla/bla/bla + terminnating 0
+    size_t buf_size = strlen(home_env_var) + 1 + strlen(home_val) + 1;//HOME=/bla/bla/bla + terminating 0
     char* home_path_buf = (char*)malloc(buf_size);
     snprintf(home_path_buf, buf_size,"%s=%s", home_env_var, home_val);
     char *env[] = {
@@ -141,13 +211,15 @@ void launch_app(AppSettings* settings, int argc, char** argv) {
 
     if (has_public_ipv4_addr) {
         new_argv[i] = strdup("--l0_public_ipv4");
-        new_argv[i + 1] = strdup(settings->public_ipv4);
+        new_argv[i + 1] = settings->public_ipv4;
+        settings->public_ipv4 = NULL;
         i += 2;
     }
 
     if (has_public_ipv6_addr) {
         new_argv[i] = strdup("--l0_public_ipv6");
-        new_argv[i + 1] = strdup(settings->public_ipv6);
+        new_argv[i + 1] = settings->public_ipv6;
+        settings->public_ipv6 = NULL;
         i += 2;
     }
 
