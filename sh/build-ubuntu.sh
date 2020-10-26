e684a2e8d3631e00727f9d47ad6d58eaa546c826
max <mpano91@gmail.com>
2020-10-26 17:20:49 +0300

    ...

--- sh/build-ubuntu.sh
+++ sh/build-ubuntu.sh
@@ -3,17 +3,17 @@
 # Set project dependence to make Teonet project from sources at empty host
 
 # Upgrade Ubuntu
-sudo apt-get update
-sudo apt-get -y upgrade
+apt-get update
+apt-get -y upgrade
 
 # Autoconf dependence
-sudo apt-get -y install autoconf intltool libtool libglib2.0-dev doxygen make gcc 
+apt-get -y install autoconf intltool libtool libglib2.0-dev doxygen make gcc 
 
 # Project dependence
-sudo apt-get install -y libssl-dev libev-dev libconfuse-dev uuid-dev libcunit1-dev cpputest libcurl4-openssl-dev
+apt-get install -y libssl-dev libev-dev libconfuse-dev uuid-dev libcunit1-dev cpputest libcurl4-openssl-dev
 
 # Libtuntap dependence
-sudo apt-get install -y cmake g++ unzip
+apt-get install -y cmake g++ unzip
 
 # Get libtuntap
 cd distr
@@ -23,11 +23,11 @@ cd libtuntap-master
 # Libtuntap build
 cmake ./
 make
-sudo make install
+make install
 
 # Remove libtuntap source
 cd ..
 rm -fr libtuntap-master
 
 # Update system dynamic libraries configuration
-sudo ldconfig
+ldconfig
