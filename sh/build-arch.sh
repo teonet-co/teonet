de5df9d3fa17568a8851d687c49d9daaff5ca90c
Kirill Scherba <kirill@scherba.ru>
2021-03-13 18:36:29 +0300

    Local 'libtuntap' version supported

--- sh/build-arch.sh
+++ sh/build-arch.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # system dependencies
-sudo pacman -Sy --noconfirm base-devel
+sudo pacman -Sy --noconfirm base-devel cmake
 sudo pacman -S --noconfirm intltool doxygen
-sudo pacman -S --noconfirm libev confuse 
+sudo pacman -S --noconfirm libev confuse cunit
 
 # cpputest
 cd distr/arch/cpputest
@@ -12,11 +12,19 @@ rm -rf pkg src cpputest-3.8*
 cd ../../..
 
 # tuntap
+#
+# package
 cd distr/arch/libtuntap
 cp ../../libtuntap.zip ./
 makepkg -si --noconfirm
 rm -rf pkg src libtuntap*
 cd ../../..
+#
+# local source code
+cd libs/libtuntap
+patch < ../../distr/arch/libtuntap/CMakeLists.txt-2.patch
+cmake ./
+cd ../..
 
 #export TEONET_HOME=`pwd`
 
